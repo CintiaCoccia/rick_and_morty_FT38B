d63cae99aefc62ab58254756ecdeb26c7ba78ab9
Cintia Coccia <cintia@MacBook-Pro-de-Cintia.local>
2023-05-19 02:11:57 -0300

    fix css import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import App from './App'
 import { BrowserRouter } from "react-router-dom"; 
 import { createRoot } from 'react-dom/client';
+import "./index.css";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement)
